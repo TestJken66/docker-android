90a0acc071e8530e9331d75ae89b2e81d5f764de
hhhaiai <sanbo.xyz@gmail.com>
2021-11-03 17:19:21 +0800

    Update rs.sh

--- rs.sh
+++ rs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # 8.1
-#docker build -t budtmo/docker-android-x86-8.1:latest  --build-arg TOKEN= --build-arg ANDROID_VERSION=8.1 --build-arg API_LEVEL=27 --build-arg PROCESSOR=x86 --build-arg SYS_IMG=x86 --build-arg IMG_TYPE=google_apis --build-arg BROWSER=chrome --build-arg CHROME_DRIVER=2.33 --build-arg APP_RELEASE_VERSION= -f docker/Emulator_x86 .
+docker build -t budtmo/docker-android-x86-8.1:latest  --build-arg TOKEN= --build-arg ANDROID_VERSION=8.1 --build-arg API_LEVEL=27 --build-arg PROCESSOR=x86 --build-arg SYS_IMG=x86 --build-arg IMG_TYPE=google_apis --build-arg BROWSER=chrome --build-arg CHROME_DRIVER=2.33 --build-arg APP_RELEASE_VERSION= -f docker/Emulator_x86 .
